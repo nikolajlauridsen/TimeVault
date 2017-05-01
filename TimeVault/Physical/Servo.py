697ea27b4e3e880fbc8143cfd5380757c9bf1c33
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-05-01 21:26:24 +0200

    Cleanup

--- TimeVault/Physical/Servo.py
+++ TimeVault/Physical/Servo.py
@@ -6,7 +6,6 @@ SERVO_PIN = 18
 class Servo:
     """Control a servo"""
     def __init__(self):
-        GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.OUT)
         self.pwm = GPIO.PWM(18, 100)
         self.pwm.start(0)
